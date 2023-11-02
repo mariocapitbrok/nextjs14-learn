4af3a1662cdb054cd6aee8db1985e5d744cd0701
Mario Capitbrok <mario@capitbrok.com>
2023-11-02 03:18:36 -0600

    Update Next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'nextjs14-learn.vercel.app',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
